51dad29a697e3001774ead5babda46c8c176806d
ksg159 <ksg9396@naver.com>
2016-11-09 17:12:17 +0900

    11월9일 김승기

---
+++ Assets/Scripts/Effect/Meteor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meteor : MonoBehaviour {
+
+
+    void Update()
+    {
+        transform.Translate(0, 0, 15 * Time.smoothDeltaTime, Space.Self);    
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if(coll.gameObject.layer == LayerMask.NameToLayer("Map"))
+        {
+            Destroy(gameObject);
+            Instantiate(Resources.Load<GameObject>("Effect/MeteorExplosion"), coll.contacts[0].point, Quaternion.identity);
+        }
+    }
+}
