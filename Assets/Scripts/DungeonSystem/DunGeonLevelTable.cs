31c5da0c2e61d949bcd9d46aa9bc0552ab0d5149
sjk98027 <kakrkddusdn@naver.com>
2016-11-28 21:31:45 +0900

    2016/11/28 던전 레벨테이블 공부(주석친거제외하고 일단 변경해봄)

--- Assets/Scripts/DungeonSystem/DunGeonLevelTable.cs
+++ Assets/Scripts/DungeonSystem/DunGeonLevelTable.cs
@@ -2,144 +2,371 @@
 using System.Collections;
 using System.Collections.Generic;
 
-//rongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrongrong
-public enum DungeonType
-{
-	None = 0,
-	Weapon,
-	Helmet,
-	Armor,
-	Gloves,
-	Shoes,
-	Ring,
-	Necklace,
+public enum MonsterName{
+	None=0,
+	Duck,
+	Frog,
+	Rabbit,
+	Bear
+}
+public class MonsterBaseData{
+	int id;
+	public int Id{get { return id;}}
+	List<MonsterStatusData> monsterStatusData;
+
+	public List<MonsterStatusData> MonsterStatusData { get { return monsterStatusData; } }
+
+	public MonsterBaseData()
+	{
+		id = 0;
+		monsterStatusData = new List<MonsterStatusData>();
+	}
+
+	public MonsterBaseData(int _id){
+		id = _id;
+		monsterStatusData = new List<MonsterStatusData> ();
+	}
+
+//	public MonsterBaseData(int newId, float newAttackRange, float newColliderSize)
+//	{
+//		Id = newId;
+//		monsterStatusData = new List<MonsterStatusData>();
+//	}
+//
+//	public MonsterBaseData(MonsterStatusData newBaseData)
+//	{
+//		Id = newBaseData.ID;
+//		monsterStatusData = new List<MonsterStatusData>();
+//	}
+
+	public MonsterStatusData GetStatusData(int level)
+	{
+		if (monsterStatusData.Count >= level)
+		{
+			return monsterStatusData[level];
+		}
+
+		return null;
+	}
+
+	public bool AddStatusData(MonsterStatusData newHeroStatusData)
+	{
+		try
+		{
+			monsterStatusData.Add(newHeroStatusData);
+			return true;
+		}
+		catch
+		{
+			return false;
+		}
+	}
+
+//	public bool RemoveStatusData(int level)
+//	{
+//		int index = FindLevelDataIndex(level);
+//
+//		if (index != -1)
+//		{
+//			try
+//			{
+//				monsterStatusData.Remove(monsterStatusData[index]);
+//				return true;
+//			}
+//			catch
+//			{
+//				return false;
+//			}
+//		}
+//		else
+//		{
+//			return false;
+//		}
+//	}
+//
+//	public bool ChangeStatusData(MonsterStatusData heroStatusData)
+//	{
+//		int index = FindStatusDataIndex(heroStatusData.Level);
+//
+//		if (index != -1)
+//		{
+//			try
+//			{
+//				statusData[index] = heroStatusData;
+//				return true;
+//			}
+//			catch
+//			{
+//				return false;
+//			}
+//		}
+//		else
+//		{
+//			return false;
+//		}
+//	}
+//
+//	public int FindStatusDataIndex(int level)
+//	{
+//		for (int i = 0; i < StatusData.Count; i++)
+//		{
+//			if (StatusData[i].Level == level)
+//			{
+//				return i;
+//			}
+//		}
+//
+//		return -1;
+//	}
+
 }
 
-//dreamStoneCount
-
-public class DungeonDatabase
+public class MonsterDataTotal
 {
-	private static DungeonDatabase instance;
+	private static MonsterDataTotal instance;
 
-	public static DungeonDatabase Instance
+	public static MonsterDataTotal Instance
 	{
 		get
 		{
 			if (instance == null)
 			{
-				instance = new DungeonDatabase();
+				instance = new MonsterDataTotal();
 			}
 
 			return instance;
 		}        
 	}
+	//public Dictionary<int, MonsterData> MonsterData;
 
-	public Dictionary<int, Dungeon> DungeonData;
+	List<MonsterBaseData> MonsterData;
+
+//	public Dictionary<int, Dungeon> DungeonData;
 
 	public void InitializeDungeonDatabase()
 	{
-		DungeonData = new Dictionary<int, Dungeon>();
-		//DungeonData.Add ();
-//		DungeonData.Add(0, new Dungeon(0, DungeonType.None, "빈칸", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(1, new Dungeon(1, DungeonType.Weapon, "작은검", 100, 2, 0, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(2, new Dungeon(2, DungeonType.Weapon, "검", 200, 3, 0, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(3, new Dungeon(3, DungeonType.Weapon, "대검", 300, 5, 0, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(4, new Dungeon(4, DungeonType.Weapon, "화염의 검", 500, 8, 0, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(5, new Dungeon(5, DungeonType.Weapon, "드래곤뼈 검", 1000, 11, 0, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(6, new Dungeon(6, DungeonType.Helmet, "가죽 모자", 100, 0, 0, 0, 3, 0, 0, 0, 0, 0));
-//		DungeonData.Add(7, new Dungeon(7, DungeonType.Helmet, "녹슨 투구", 200, 0, 0, 0, 6, 0, 0, 0, 0, 0));
-//		DungeonData.Add(8, new Dungeon(8, DungeonType.Helmet, "강철 투구", 300, 0, 0, 0, 10, 0, 0, 0, 0, 0));
-//		DungeonData.Add(9, new Dungeon(9, DungeonType.Helmet, "미스릴 투구", 500, 0, 0, 0, 15, 0, 0, 0, 0, 0));
-//		DungeonData.Add(10, new Dungeon(10, DungeonType.Helmet, "드래곤비늘 투구", 1000, 0, 0, 0, 20, 0, 0, 0, 0, 0));
-//		DungeonData.Add(11, new Dungeon(11, DungeonType.Armor, "가죽 갑옷", 100, 0, 1, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(12, new Dungeon(12, DungeonType.Armor, "녹슨 갑옷", 200, 0, 2, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(13, new Dungeon(13, DungeonType.Armor, "체인 갑옷", 300, 0, 3, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(14, new Dungeon(14, DungeonType.Armor, "미스릴 갑옷", 500, 0, 4, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(15, new Dungeon(15, DungeonType.Armor, "드래곤비늘 갑옷", 1000, 0, 5, 0, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(16, new Dungeon(16, DungeonType.Gloves, "천 장갑", 100, 0, 0, 0, 0, 0, 0, 0.1f, 0, 0));
-//		DungeonData.Add(17, new Dungeon(17, DungeonType.Gloves, "가죽 장갑", 200, 0, 0, 0, 0, 0, 0, 0.15f, 0, 0));
-//		DungeonData.Add(18, new Dungeon(18, DungeonType.Gloves, "건틀릿", 300, 0, 0, 0, 0, 0, 0, 0.2f, 0, 0));
-//		DungeonData.Add(19, new Dungeon(19, DungeonType.Gloves, "강철 건틀릿", 500, 0, 0, 0, 0, 0, 0, 0.25f, 0, 0));
-//		DungeonData.Add(20, new Dungeon(20, DungeonType.Gloves, "드래곤비늘 건틀릿", 1000, 0, 0, 0, 0, 0, 0, 0.3f, 0, 0));
-//		DungeonData.Add(21, new Dungeon(21, DungeonType.Shoes, "천 신발", 100, 0, 0, 0, 0, 0, 0.1f, 0, 0, 0));
-//		DungeonData.Add(22, new Dungeon(22, DungeonType.Shoes, "가죽 신발", 200, 0, 0, 0, 0, 0, 0.15f, 0, 0, 0));
-//		DungeonData.Add(23, new Dungeon(23, DungeonType.Shoes, "털 신발", 300, 0, 0, 0, 0, 0, 0.2f, 0, 0, 0));
-//		DungeonData.Add(24, new Dungeon(24, DungeonType.Shoes, "강철 신발", 500, 0, 0, 0, 0, 0, 0.25f, 0, 0, 0));
-//		DungeonData.Add(25, new Dungeon(25, DungeonType.Shoes, "드래곤비늘 신발", 1000, 0, 0, 0, 0, 0, 0.3f, 0, 0, 0));
-//		DungeonData.Add(26, new Dungeon(26, DungeonType.Ring, "돌 반지", 100, 0, 0, 1, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(27, new Dungeon(27, DungeonType.Ring, "은 반지", 200, 0, 0, 2, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(28, new Dungeon(28, DungeonType.Ring, "금 반지", 300, 0, 0, 3, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(29, new Dungeon(29, DungeonType.Ring, "다이아몬드 반지", 500, 0, 0, 4, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(30, new Dungeon(30, DungeonType.Ring, "마법 깃든 반지", 1000, 0, 0, 5, 0, 0, 0, 0, 0, 0));
-//		DungeonData.Add(31, new Dungeon(31, DungeonType.Necklace, "돌 목걸이", 100, 0, 0, 0, 0, 0, 0, 0, 1, 0));
-//		DungeonData.Add(32, new Dungeon(32, DungeonType.Necklace, "은 목걸이", 200, 0, 0, 0, 0, 0, 0, 0, 0, 1));
-//		DungeonData.Add(33, new Dungeon(33, DungeonType.Necklace, "금 목걸이", 300, 0, 0, 0, 0, 0, 0, 0, 1, 2));
-//		DungeonData.Add(34, new Dungeon(34, DungeonType.Necklace, "다이아몬드 목걸이", 500, 0, 0, 0, 0, 0, 0, 0, 1, 3));
-//		DungeonData.Add(35, new Dungeon(35, DungeonType.Necklace, "마법 깃든 목걸이", 1000, 0, 0, 0, 0, 0, 0, 0, 2, 3));
+
+		MonsterData = new List<MonsterBaseData> ();
+		MonsterData.Add (new MonsterBaseData ((int)MonsterName.None));
+		MonsterData.Add (new MonsterBaseData ((int)MonsterName.Duck));
+		MonsterData.Add (new MonsterBaseData ((int)MonsterName.Frog));
+		MonsterData.Add (new MonsterBaseData ((int)MonsterName.Rabbit));
+		MonsterData.Add (new MonsterBaseData ((int)MonsterName.Bear));
+
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (1, "Duck", 1, 2, 900, 0, 1, 0, 1));// right 3 -> X;
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (2, "Duck", 2, 3, 945, 0, 1, 0, 1));
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (3, "Duck", 3, 5, 1039, 0, 1, 0, 1));
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (4, "Duck", 4, 6, 1195, 1, 1, 0, 1));
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (5, "Duck", 5, 8, 1434, 2, 1, 0, 1));
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (6, "Duck", 6, 10, 1793, 2, 1, 0, 1));
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (7, "Duck", 7, 13, 2331, 3, 1, 0, 1));
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (8, "Duck", 8, 15, 3146, 3, 1, 0, 1));
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (9, "Duck", 9, 19, 4405, 4, 1, 0, 1));
+		MonsterData [(int)MonsterName.Duck].AddStatusData (new MonsterStatusData (10, "Duck", 10, 24, 6388, 5, 1, 0, 1));
+
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (1, "Frog", 1, 2, 900, 0, 1, 0, 1));// right 3 -> X;
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (2, "Frog", 2, 3, 945, 0, 1, 0, 1));
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (3, "Frog", 3, 5, 1039, 0, 1, 0, 1));
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (4, "Frog", 4, 6, 1195, 1, 1, 0, 1));
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (5, "Frog", 5, 8, 1434, 2, 1, 0, 1));
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (6, "Frog", 6, 10, 1793, 2, 1, 0, 1));
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (7, "Frog", 7, 13, 2331, 3, 1, 0, 1));
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (8, "Frog", 8, 15, 3146, 3, 1, 0, 1));
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (9, "Frog", 9, 19, 4405, 4, 1, 0, 1));
+		MonsterData [(int)MonsterName.Frog].AddStatusData (new MonsterStatusData (10, "Frog", 10, 24, 6388, 5, 1, 0, 1));
+
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (1, "Rabbit", 1, 5, 900, 0, 1, 0, 1));// right 3 -> X;
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (2, "Rabbit", 2, 7, 945, 0, 1, 0, 1));
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (3, "Rabbit", 3, 10, 1039, 0, 1, 0, 1));
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (4, "Rabbit", 4, 12, 1195, 1, 1, 0, 1));
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (5, "Rabbit", 5, 16, 1434, 2, 1, 0, 1));
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (6, "Rabbit", 6, 21, 1793, 4, 1, 0, 1));
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (7, "Rabbit", 7, 26, 2331, 8, 1, 0, 1));
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (8, "Rabbit", 8, 31, 3146, 9, 1, 0, 1));
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (9, "Rabbit", 9, 38, 4405, 12, 1, 0, 1));
+		MonsterData [(int)MonsterName.Rabbit].AddStatusData (new MonsterStatusData (10, "Rabbit", 10, 48, 6388, 15, 1, 0, 1));
+
+		MonsterData [(int)MonsterName.Bear].AddStatusData (new MonsterStatusData (1, "Bear", 1, 10, 4500, 0, 1, 1, 115));// right 1 -> exp; 2-> stone ; 3-> moveSpeed;
+		MonsterData[(int)MonsterName.Bear].AddStatusData(new MonsterStatusData(2,"Bear",2,15,4725,0,1,2,309));	
+		MonsterData[(int)MonsterName.Bear].AddStatusData(new MonsterStatusData(3,"Bear",3,20,5198,0,1,3,425));	
+		MonsterData[(int)MonsterName.Bear].AddStatusData(new MonsterStatusData(4,"Bear",4,25,5977,2,1,4,596));	
+		MonsterData[(int)MonsterName.Bear].AddStatusData(new MonsterStatusData(5,"Bear",5,33,7173,4,1,5,710));
+		MonsterData[(int)MonsterName.Bear].AddStatusData(new MonsterStatusData(6,"Bear",6,42,8966,8,1,6,825));
+		MonsterData[(int)MonsterName.Bear].AddStatusData(new MonsterStatusData(7,"Bear",7,52,11655,12,1,8,1065));
+		MonsterData[(int)MonsterName.Bear].AddStatusData(new MonsterStatusData(8,"Bear",8,63,15735,18,1,10,1456));
+		MonsterData[(int)MonsterName.Bear].AddStatusData(new MonsterStatusData(9,"Bear",9,77,22029,24,1,12,1857));
+		MonsterData[(int)MonsterName.Bear].AddStatusData(new MonsterStatusData(10,"Bear",10,97,31942,30,1,16,2483));
+	}
+
+	public MonsterBaseData GetHeroData(int Id)
+		{
+		if(MonsterData.Count > Id)
+			{
+			for(int i = 0; i< MonsterData.Count; i++)
+				{
+				if(MonsterData[i].Id == Id)
+					{
+					return MonsterData[i];
+					}
+				}
+				return null;
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+}
+
+
+
+public class MonsterStatusData{
+	int id;
+	string monsterName;
+	int monsterLevel;
+	int monsterDamage;
+	int monsterHp;
+	int monsterDefense;
+	float monsterMoveSpeed;
+	int dreamStoneCount;
+	int monsterExp;
+
+
+	public int ID { get { return id; } }
+	public string MonsterName{get{ return monsterName;}}
+	public int MonsterLevel{get{ return monsterLevel;}}
+	public int MonsterDamage{ get { return monsterDamage; } }
+	public int MonsterHp{get{ return monsterHp;}}
+	public int MonsterDefense{get{ return monsterDefense;}}
+	public int DreamStoneCount{get{ return dreamStoneCount;}}
+	public int MonsterExp{get{ return monsterExp;}}
+
+	public float MonsterMoveSpeed{get{ return monsterMoveSpeed;}}
+
+	public MonsterStatusData(){
+		id = 0;
+		monsterName = "니가가라화와이";
+		monsterLevel = 1;
+		monsterDamage = 0;
+		monsterHp = 0;
+		monsterDefense = 0;
+		monsterMoveSpeed = 1;
+		dreamStoneCount = 1;
+		monsterExp = 115;
+	}
+
+	public MonsterStatusData(int _id,
+		string _monsterName,
+		int _monsterLevel,
+		int _monsterDamage,
+		int _monsterHp,
+		int _monsterDefense,
+		float _monsterMoveSpeed,
+		int _dreamStoneCount,
+		int _monsterExp
+
+	)
+	{
+		id = _id;
+		monsterName = _monsterName;
+		monsterLevel = _monsterLevel;
+		monsterDamage = _monsterDamage;
+		monsterHp = _monsterHp;
+		monsterDefense = _monsterDefense;
+		monsterMoveSpeed = _monsterMoveSpeed;
+		dreamStoneCount = _dreamStoneCount;
+		monsterExp = _monsterExp;
 	}
 }
 
-public class Dungeon
+
+
+public enum UnitID
 {
-	int Id;
-	DungeonType type;
-	string name;
-	int dreamStoneCount;
-	int attack;
-	int defense;
-	int magicDefense;
-	int health;
-	int mana;
-	float moveSpeed;
-	float attackSpeed;
-	int healthRegeneration;
-	int manaRegeneration;
+	None,
+	Unity,
 
-	public int ID { get { return Id; } }
-	public DungeonType Type { get { return type; } }
-	public string Name { get { return name; } }
-	public int DreamStoneCount { get { return dreamStoneCount; } }
-	public int Attack { get { return attack; } }
-	public int Defense { get { return defense; } }
-	public int MagicDefense { get { return magicDefense; } }
-	public int Health { get { return health; } }
-	public int Mana { get { return mana; } }
-	public float MoveSpeed { get { return moveSpeed; } }
-	public float AttackSpeed { get { return attackSpeed; } }
-	public int HealthRegeneration { get { return healthRegeneration; } }
-	public int ManaRegeneration { get { return manaRegeneration; } }
+}
+//
+//public enum State
+//{
+//	Idle = 1,
+//	Move,
+//	Attack,
+//	Die,
+//	Skill1,
+//	Skill2,
+//	Skill3,
+//}
 
-//	public Dungeon()
+//public class HeroDatabase
+//{
+//	private static HeroDatabase instance;
+//
+//	public static HeroDatabase Instance
 //	{
-//		Id = 0;
-//		type = DungeonType.None;
-//		gold = 0;
-//		attack = 0;
-//		defense = 0;
-//		magicDefense = 0;
-//		health = 0;
-//		mana = 0;
-//		moveSpeed = 0;
-//		attackSpeed = 0;
-//		healthRegeneration = 0;
-//		manaRegeneration = 0;
+//		get
+//		{
+//			if(instance == null)
+//			{
+//				instance = new HeroDatabase();
+//			}
+//
+//			return instance;
+//		}
 //	}
 //
-//	public Dungeon(int newId, DungeonType newType, string newName, int newGold, int newAttack, int newDefense, int newMagicDefense, int newHealth, int newMana,
-//		float newMoveSpeed, float newAttackSpeed, int newHealthRegeneration, int newManaRegeneration)
+//	List<HeroBaseData> HeroData;
+//
+//	public void InitializeHeroDatabase()
 //	{
-//		Id = newId;
-//		type = newType;
-//		name = newName;
-//		gold = newGold;
-//		attack = newAttack;
-//		defense = newDefense;
-//		magicDefense = newMagicDefense;
-//		health = newHealth;
-//		mana = newMana;
-//		moveSpeed = newMoveSpeed;
-//		attackSpeed = newAttackSpeed;
-//		healthRegeneration = newHealthRegeneration;
-//		manaRegeneration = newManaRegeneration;
+//		HeroData = new List<HeroBaseData>();
+//
+//		//이름, 공격범위, 충돌범위 
+//		HeroData.Add(new HeroBaseData((int)UnitID.None, 1.0f, 1.0f));
+//		HeroData.Add(new HeroBaseData((int)UnitID.Unity, 1.0f, 1.0f));
+//
+//		//레벨, 경험치, 공격력, 방어력, 마법방어력, 체력, 마나, 이동속도, 공격속도, 체력리젠, 마나리젠
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(1, 100, 5, 0, 0, 40, 5, 5, 0.8f, 1, 0, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(2, 200, 7, 0, 0, 45, 7, 5, 0.85f, 1, 0, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(3, 400, 9, 0, 0, 50, 9, 5, 0.9f, 1, 0, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(4, 750, 11, 0, 0, 55, 11, 5, 0.95f, 1, 0, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(5, 1300, 13, 0, 0, 60, 13, 5, 1f, 1, 1, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(6, 2100, 15, 0, 0, 65, 15, 5, 1.05f, 1, 1, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(7, 3200, 17, 1, 0, 71, 17, 5, 1.1f, 1, 1, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(8, 4650, 19, 2, 0, 77, 19, 5, 1.15f, 1, 1, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(9, 6500, 21, 3, 0, 83, 21, 5, 1.2f, 1, 2, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(10, 8800, 23, 4, 0, 89, 23, 5, 1.25f, 1, 2, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(11, 11600, 25, 5, 0, 95, 26, 5, 1.3f, 1, 2, 0));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(12, 14950, 27, 6, 1, 102, 29, 5, 1.35f, 1, 2, 1));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(13, 18900, 29, 7, 2, 109, 32, 5, 1.4f, 1, 3, 1));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(14, 23500, 31, 8, 3, 116, 35, 5, 1.45f, 1, 3, 1));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(15, 28800, 33, 9, 4, 123, 38, 5, 1.5f, 1, 3, 1));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(16, 34850, 35, 10, 5, 130, 41, 5, 1.55f, 1, 3, 1));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(17, 41700, 37, 12, 6, 138, 44, 5, 1.6f, 1, 4, 1));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(18, 49400, 39, 14, 7, 146, 47, 5, 1.65f, 1, 4, 1));
+//		HeroData[(int)UnitID.Unity].AddLevelData(new HeroLevelData(19, 58000, 41, 16, 9, 154, 50, 5, 1.7f, 1, 4, 1));
 //	}
-}
+//
+//	public HeroBaseData GetHeroData(int Id)
+//	{
+//		if(HeroData.Count > Id)
+//		{
+//			for(int i = 0; i< HeroData.Count; i++)
+//			{
+//				if(HeroData[i].ID == Id)
+//				{
+//					return HeroData[i];
+//				}
+//			}
+//			return null;
+//		}
+//		else
+//		{
+//			return null;
+//		}
+//	}
+//}
+//
+//
