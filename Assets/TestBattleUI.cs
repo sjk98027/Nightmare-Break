bdc1e8915aa54122ba8195b9851a903db0b95dcb
ksg159 <ksg9396@naver.com>
2016-11-29 15:36:17 +0900

    11월 29일 오후 UIManager수정

---
+++ Assets/TestBattleUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBattleUI : MonoBehaviour {
+
+    BattleUIManager battleUI;
+
+	// Use this for initialization
+	void Start () {
+        battleUI = new BattleUIManager();
+        battleUI.SetUIObject();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
