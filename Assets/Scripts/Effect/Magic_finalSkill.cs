4d64bfb9409c7295c3e7189a34ca870be3ba070d
ksg159 <ksg9396@naver.com>
2016-11-30 16:48:36 +0900

    11/30 오후 커밋

---
+++ Assets/Scripts/Effect/Magic_finalSkill.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magic_finalSkill : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
