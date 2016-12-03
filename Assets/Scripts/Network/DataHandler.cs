6341634021374adaacf430449356026d7c47dde0
eslia0 <eslia0@naver.com>
2016-12-03 18:29:04 +0900

    Remove 수정

--- Assets/Scripts/Network/DataHandler.cs
+++ Assets/Scripts/Network/DataHandler.cs
@@ -372,7 +372,8 @@ public class DataHandler : MonoBehaviour
 
         try
         {
-            networkManager.ReSendManager.RemoveReSendData(udpId, packet.endPoint);
+            SendData sendData = new SendData(packet.endPoint, udpId);
+            networkManager.ReSendManager.RemoveReSendData(sendData);
         }
         catch
         {
