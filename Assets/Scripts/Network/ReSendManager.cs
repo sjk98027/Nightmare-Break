face540d12c5bcb5efa683c686c127535754f99e
eslia0 <eslia0@naver.com>
2016-12-05 17:10:17 +0900

    다시 보내는 시간 2초 늘림

--- Assets/Scripts/Network/ReSendManager.cs
+++ Assets/Scripts/Network/ReSendManager.cs
@@ -68,7 +68,7 @@ public class ReSendManager : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(3.0f);
 
             Debug.Log("ReSendData 체크");
 
