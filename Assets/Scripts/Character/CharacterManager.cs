9e17ddb650241158372da7c96b0ea41ed480e881
ksg159 <ksg9396@naver.com>
2016-12-06 18:49:57 +0900

    파일정리

--- Assets/Scripts/Character/CharacterManager.cs
+++ Assets/Scripts/Character/CharacterManager.cs
@@ -64,7 +64,7 @@ public class CharacterManager : MonoBehaviour
 	void Start ()
 	{
 		SetCharacterStatus ();
-		uiManager = GameObject.FindWithTag("UIManager").GetComponent<UIManager>();
+		//uiManager = GameObject.FindWithTag("UIManager").GetComponent<UIManager>();
 		animator = GetComponent<Animator> ();
 		state = CharacterState.Idle;
 		rigdbody = GetComponent<Rigidbody> ();
