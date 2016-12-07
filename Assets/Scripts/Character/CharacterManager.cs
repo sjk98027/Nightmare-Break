598fc8d3a60617a562eb2c2786474ce47fdc423a
eslia0 <eslia0@naver.com>
2016-12-07 20:24:53 +0900

    캐릭터 애니메이션

--- Assets/Scripts/Character/CharacterManager.cs
+++ Assets/Scripts/Character/CharacterManager.cs
@@ -433,8 +433,8 @@ public class CharacterManager : MonoBehaviour
 
             if (userNum == 0)
             {
-                //DataSender.Instance.CharacterActionSend(Inputstate);
-            }			
+                DataSender.Instance.CharacterActionSend(Inputstate);
+            }
 		}
 	}
 
