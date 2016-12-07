34e6c7b4637c4c01e5464c0c1abc1d2e51ac4c9d
eslia0 <eslia0@naver.com>
2016-12-07 20:19:53 +0900

    0.016

--- Assets/Scripts/Network/DataSender.cs
+++ Assets/Scripts/Network/DataSender.cs
@@ -410,7 +410,7 @@ public class DataSender : MonoBehaviour
 
         while (true)
         {
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.016f);
 
             Debug.Log("번호 : " + characterManager.UserNum);
 
