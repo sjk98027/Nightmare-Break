b5bc93b13192fdf84602702bdde0bd373a3f86ae
eslia0 <eslia0@naver.com>
2016-12-09 13:50:51 +0900

    던전매니저 스타트 없앰

--- Assets/Scripts/Database/HeroDatabase.cs
+++ Assets/Scripts/Database/HeroDatabase.cs
@@ -37,26 +37,26 @@ public class HeroDatabase
         AddBaseData(new HeroBaseData((int)HeroId.Mage, "Mage"));
 
         GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(1, 10, 0, 150, 100, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(2, 7, 1, 220, 60, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(3, 9, 2, 300, 70, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(4, 12, 3, 390, 110, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(5, 15, 5, 490, 150, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(6, 18, 7, 600, 190, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(7, 21, 9, 720, 230, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(8, 25, 11, 850, 270, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(9, 29, 13, 990, 310, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(10, 33, 15, 1240, 350, 1, 1, 7, 100));
+        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(2, 7, 1, 220, 130, 1, 1, 7, 250));
+        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(3, 9, 2, 300, 170, 2, 1, 7, 450));
+        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(4, 12, 3, 390, 220, 2, 1, 7, 750));
+        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(5, 15, 5, 490, 280, 3, 2, 7, 1250));
+        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(6, 18, 7, 600, 350, 3, 2, 7, 2000));
+        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(7, 21, 9, 720, 430, 4, 2, 7, 3000));
+        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(8, 25, 11, 850, 520, 4, 2, 7, 4500));
+        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(9, 29, 13, 990, 620, 5, 3, 7, 7000));
+        GetBaseData((int)HeroId.Warrior).AddLevelData(new HeroLevelData(10, 33, 15, 1240, 730, 5, 3, 7, 14000));
 
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(1, 3, 0, 35, 4, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(2, 4, 0, 45, 4, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(3, 5, 0, 55, 4, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(4, 6, 0, 75, 5, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(5, 7, 0, 95, 5, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(6, 8, 0, 115, 5, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(7, 10, 0, 145, 5, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(8, 12, 0, 175, 5, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(9, 14, 0, 205, 6, 1, 1, 7, 100));
-        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(10, 16, 0, 235, 6, 1, 1, 7, 100));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(1, 12, 0, 120, 150, 1, 1, 7, 100));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(2, 15, 0, 170, 190, 1, 1, 7, 250));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(3, 19, 0, 230, 240, 1, 2, 7, 450));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(4, 24, 0, 300, 300, 1, 2, 7, 750));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(5, 30, 1, 380, 370, 1, 3, 7, 1250));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(6, 37, 2, 470, 450, 1, 3, 7, 2000));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(7, 45, 3, 570, 540, 2, 4, 7, 3000));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(8, 54, 4, 680, 640, 2, 4, 7, 4500));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(9, 64, 5, 800, 750, 2, 5, 7, 7000));
+        GetBaseData((int)HeroId.Mage).AddLevelData(new HeroLevelData(10, 75, 6, 930, 870, 2, 6, 7, 14000));
     }
 
     public bool AddBaseData(HeroBaseData newHeroData)
