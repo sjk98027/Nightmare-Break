9753c74536669bee731d8573302fb5188440b70d
eslia0 <eslia0@naver.com>
2016-12-09 13:51:10 +0900

    캐릭터 웨폰 주석

--- Assets/Scripts/Character/CharWeapon.cs
+++ Assets/Scripts/Character/CharWeapon.cs
@@ -87,12 +87,12 @@ public class CharWeapon : MonoBehaviour
                 }
             }
         }
-        else {
-            ComboSystem.instance.ComboProcess(++charManager.ComboCount);
-            StartCoroutine(charManager.ComboCheck(charManager.ComboCount));
-			ComboSystem.instance.ComboAction(Camera.main.transform);
-            Instantiate(Resources.Load<GameObject>("Effect/HitEffect"), new Vector3(coll.transform.position.x, coll.transform.position.y + 1.0f, coll.transform.position.z + 0.5f), Quaternion.identity);
+   //     else {
+   //         ComboSystem.instance.ComboProcess(++charManager.ComboCount);
+   //         StartCoroutine(charManager.ComboCheck(charManager.ComboCount));
+			//ComboSystem.instance.ComboAction(Camera.main.transform);
+   //         Instantiate(Resources.Load<GameObject>("Effect/HitEffect"), new Vector3(coll.transform.position.x, coll.transform.position.y + 1.0f, coll.transform.position.z + 0.5f), Quaternion.identity);
         
-        }
+   //     }
     }
 }
