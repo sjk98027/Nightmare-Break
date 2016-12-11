f6dae10eeb2770227c416ec33ea617aa33f66bb0
Seung-PC\Seung <ksg9396@naver.com>
2016-12-12 05:11:10 +0900

    커밋

---
+++ Assets/Scripts/Database/XmlSkillData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using System.Xml;
+
+public class XmlSkillData : MonoBehaviour {
+
+	[XmlAttribute("name")]
+	public string name;
+
+
+
+}
