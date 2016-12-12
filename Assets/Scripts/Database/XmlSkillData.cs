9808458ad2759040ac5405dea556d254471af879
ksg159 <ksg9396@naver.com>
2016-12-12 15:32:07 +0900

    미안하다!!!!!!!!!!!

--- Assets/Scripts/Database/XmlSkillData.cs
+++ Assets/Scripts/Database/XmlSkillData.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
-using System.Collections;
-using jo
+
 
 public class XmlSkillData : MonoBehaviour {
 
-    XmlDocument skillData = new XmlDocument();
+   //mlDocument skillData = new XmlDocument();
    // XmlElement myElement = skillData.CreateElement("Skill");
 
 
